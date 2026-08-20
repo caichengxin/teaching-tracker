74b0b298c0a34dd7b834bc0851bd253e9b30a7ce
caichengxin <chengxin.tsai@gmail.com>
2026-08-20 12:42:13 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Browser-safe configuration only. Never put Supabase secret/service_role keys here.
-window.APP_CONFIG={supabaseUrl:"",supabasePublishableKey:"",storageBucket:"lecture-files",vapidPublicKey:"",appName:"Teaching Tracker"};
+window.APP_CONFIG={supabaseUrl:"",supabasePublishableKey:"",storageBucket:"lecture-files",vapidPublicKey:"BO379XSNglaSlDLVGP1HpL7vMCV0224Zl_F-kD32DDUcBDJOlRrZ7qh70V1GkbzOchBcVD501UwezvouAKAqcmQ",appName:"Teaching Tracker"};
